b25b451c3f0fdd07d8da59c283c932f51ebb5ee4
Evan Corley <eccorley@gmail.com>
2025-05-13 00:15:12 -0500

    tweaks

---
+++ modules/colorschemes/catppuccin.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+{
+  colorschemes.catppuccin = {
+    enable = true;
+    settings = {
+      flavour = "mocha";
+      transparent_background = config.nvix.transparent;
+      no_italic = true;
+      styles = {
+        floats = if config.nvix.transparent then "transparent" else "dark";
+        sidebars = if config.nvix.transparent then "transparent" else "dark";
+      };
+      integrations = {
+        cmp = true;
+        gitsigns = true;
+        treesitter = true;
+        notify = false;
+        mini = {
+          enabled = true;
+          indentscope_color = "";
+        };
+      };
+    };
+  };
+}
